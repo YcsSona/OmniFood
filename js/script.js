2ae2d111e8d0be8743f79c93aa168d5804f2f39d
ycssona <ycssona@gmail.com>
2023-08-18 12:29:38 +0530

    Implementing sticky navigation bar

--- js/script.js
+++ js/script.js
@@ -50,3 +50,28 @@ allLinks.forEach(function (link) {
       headerEl.classList.toggle("nav-open");
   });
 });
+
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entires) {
+    const ent = entires[0];
+
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
