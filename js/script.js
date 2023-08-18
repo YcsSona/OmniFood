13d8629047f765253aaac90a78dc6545bb578985
ycssona <ycssona@gmail.com>
2023-08-18 11:09:14 +0530

    Implementing smooth scrolling

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@
 
 // const h1 = document.querySelector(".heading-primary");
 
-
-
 // h1.addEventListener("click", function(){
 //   h1.textContent = myName;
 //   h1.style.backgroundColor="teal";
@@ -14,12 +12,41 @@
 
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
-yearEl.textContent=currentYear;
+yearEl.textContent = currentYear;
 
 // Make mobile nav work
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 
-btnNavEl.addEventListener("click", function(){
+btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
+});
+
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
 });
